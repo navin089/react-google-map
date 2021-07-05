9343ab77e1f9d79171545ab33235376bc901d6f0
unknown <navinkodam11@gmail.com>
2021-07-05 13:51:04 +0530

    basic polygon drawn on google map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Map, Polygon, GoogleApiWrapper } from "google-maps-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export class App extends Component {
+  render() {
+    const triangleCoords = [
+      { lat: 25.774, lng: -80.19 },
+      { lat: 18.466, lng: -66.118 },
+      { lat: 32.321, lng: -64.757 },
+      { lat: 25.774, lng: -80.19 },
+    ];
+
+    return (
+      <div>
+        <Map
+          google={this.props.google}
+          style={{ width: "100%", height: "100%", position: "relative" }}
+          className={"map"}
+          zoom={14}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Polygon
+            paths={triangleCoords}
+            strokeColor="#0000FF"
+            strokeOpacity={0.8}
+            strokeWeight={2}
+            fillColor="#0000FF"
+            fillOpacity={0.35}
+          />
+        </Map>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: process.env.GOOGLE_MAPS_API_KEY,
+})(App);
---
+++ src/GoogleMap.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function GoogleMap() {
+  
+    return (
+        <div>
+                <h1>Hey beauty..!!</h1>
+            
+        </div>
+    )
+}
+
+export default GoogleMap;
